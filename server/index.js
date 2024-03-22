3357e89720c8ccb0402dc25a6bdca42447144e99
Lucas Araujo <lucasribara@gmail.com>
2024-03-22 15:17:36 -0300

    Create controller and model for User

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 //configs
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage});
+
+//routes with files
+app.post("/auth/register", upload.single("picture"), register);
 
 //mongoose setup
 
